e26266f9cb7f0c79fd226ce95d01ecd8ba2f7e46
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-11-29 16:00:10 +0300

    new project added

---
+++ WebApplication_MvcCoreCodes/WebApplication_MvcCoreCodes/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication_MvcCoreCodes.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public int Birth { get; set; }
+
+        public bool HasJob { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+    }
+}
