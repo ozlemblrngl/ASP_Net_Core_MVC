32382a04712d7bc12b8f663a50c9ff06bbbd3b0d
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-11-30 20:49:42 +0300

    convention base routing and attribute base routing Added

---
+++ WebApplication_MvcCoreCodes/WebApplication_MvcCoreCodes/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_MvcCoreCodes.Controllers
+{
+   // [Route("/Categories")] // convention based routing
+    public class CategoryController : Controller
+    {
+        // [HttpGet] ---> convention based routing
+
+        [Route("/categories")] // --> attribute base routing
+        public IActionResult List()
+        {
+            return Content("Category List");
+        }
+
+        // iki metot da get'tir eğer birine ayrıca belirtmeden kullanırsak hata alırız.
+        // ya da diğerine id li parametre veririz.
+        // [Route("{id}")]----> convention based routing
+
+        [Route ("/categories/{id}")] // --> attribute base routing
+        public IActionResult Details(int id)
+        {
+            return Content("Category List");
+        }
+    }
+}
