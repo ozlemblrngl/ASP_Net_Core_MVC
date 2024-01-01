8cf1a27a55d3bf2cb5b759976338c1466e2e7e92
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-01-01 16:36:48 +0300

    components added

---
+++ WebApplication_FirstAspNetCoreApp/Components/Announcement.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_FirstAspNetCoreApp.Components
+{
+    [ViewComponent]
+    public class Announcement : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<string> items = new List<string>
+            {
+                "lorem", "ipsum", "pactum","sanction", "lorempt"
+            };
+
+            return View(items);
+        }
+    }
+}
--- WebApplication_FirstAspNetCoreApp/Models/Customer.cs
+++ WebApplication_FirstAspNetCoreApp/Models/Customer.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication_FirstAspNetCoreApp.Models
-{ 
+{
     // Data Annotation
     public class Customer
     {
@@ -18,7 +18,7 @@ namespace WebApplication_FirstAspNetCoreApp.Models
         public string CreditCardNumber { get; set; }
 
         [Phone] // telefon numarası kontrolu yapan attribute'tür
-        public string Phone {  get; set; }
+        public string Phone { get; set; }
 
         [Url] // url kontrolu yapar. 
         public string LinkednUrl { get; set; }
@@ -26,16 +26,16 @@ namespace WebApplication_FirstAspNetCoreApp.Models
         [EmailAddress, MaxLength(50)] // mail adresi kontrolu yapar.İstersek en uzun değerini de belirtebiliriz.
         public string Email { get; set; }
 
-        [StringLength(250, MinimumLength =30)] // adresin mak uzunluğu 250, min uzunluğu 30 dur.
+        [StringLength(250, MinimumLength = 30)] // adresin mak uzunluğu 250, min uzunluğu 30 dur.
         public string Address { get; set; }
 
         [RegularExpression("[a-z]{3}-[0-9]{2}-[5-9]{4}")] // bu şekilde özel bir şart tanıyabiliriz.(regex)
         public string PostalCode { get; set; }
 
-        [Required,StringLength(24,MinimumLength =8), Compare(nameof(RePassword))] // zorunlu, max 24 min 8 karakterli ve ikinci kez girilen şifreyle karşılaştırmalı
-        public string Password { get; set; }  // Compare("RePassword") de yazabiliriz ancak o zaman RePassword'un ismi değişirse hata almayız bu kodda. nameof ile yaparsak ikinci şifrenin adı değiştiğinde bize doğrusunu yapmak üzere uyarı verir.
+        [Required, StringLength(24, MinimumLength = 8), Compare(nameof(RePassword))] // zorunlu, max 24 min 8 karakterli ve ikinci kez girilen şifreyle karşılaştırmalı
+        public string Password { get; set; }  // Compare("RePassword") de yazabiliriz ancak o zaman RePassword'un ismi değişirse hata almayız bu kodda. nameof ile yaparsak ikinci şifrenin adı değiştiğinde bize doğrusunu yapmak üzere uyarı verir.3
 
-        [Required,StringLength(24,MinimumLength =8)]
+        [Required, StringLength(24, MinimumLength = 8)]
         public string RePassword { get; set; }
 
     }
